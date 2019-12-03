b4f61ff3b4a3624123bd5c966ba5b0274f67deb2
pdvonzoo <a01088570194@gmail.com>
2019-12-03 22:43:27 +0900

    vue event

--- 1/main.js
+++ 1/main.js
@@ -4,6 +4,28 @@ const app = new Vue({
     product: "Socks!!",
     image: "./assets/vmSocks-green-onWhite.jpg",
     inStock: true,
-    inventory: 10
+    inventory: 10,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
